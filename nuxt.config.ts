bb97937ef6813b0e73d43332c7ba613bd6dc72ff
Zainab Umar Ibrahim <xieumar@gmail.com>
2026-05-11 13:19:31 -0800

    Init(Setup): Initial project setup.

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
+  typescript: {
+    strict: true
+  }
+})
